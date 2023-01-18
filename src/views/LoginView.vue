e5388bd1310ce5743b6d0a232837488c3ff0fa06
julian-saleix <julian.saleix@gmail.com>
2023-01-18 16:24:03 +0100

    Applying same style on Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -35,19 +35,66 @@ const onSubmit = async () => {
 </script>
 
 <template>
-    <div class="auth">
-        <form @submit.prevent="onSubmit">
-            <input name="email" placeholder="Addresse e-mail" type="text" v-model="formData.email" />
-            <input name="password" placeholder="Mot de passe" type="password" v-model="formData.password" />
-            <button type="submit">Se connecter</button>
-        </form>
-        <div>
-            <a href="">Mot de passe oublié</a>
-            <a href="">Pas de compte? S'inscrire</a>
+    <div class="container auth">
+        <div class="left_section">
+
+        </div>
+        <div class="right_section">
+            <h3>Login</h3>
+            <form @submit.prevent="onSubmit">
+                <input name="email" placeholder="Addresse e-mail" type="text" v-model="formData.email" />
+                <input name="password" placeholder="Mot de passe" type="password" v-model="formData.password" />
+                <button type="submit">Se connecter</button>
+            </form>
+            <div>
+                <a href="">Mot de passe oublié</a>
+                <a href="">Pas de compte? S'inscrire</a>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss" >
+.auth {
+    display: flex;
+    width: 100%;
+    padding: 2em 1em;
+    gap: 10px;
 
+    .right_section {
+        width: 60%;
+        background-color: #f9f9f9;
+        border: 1px solid var(--border);
+        padding: 4em 1em;
+        display: flex;
+        flex-direction: column;
+        gap: 0.8em;
+
+        h3 {
+            color: var(--text-dark);
+            padding: 0;
+            margin: 0;
+        }
+
+        form {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+            border-bottom: 1px solid rgb(183, 183, 183);
+            padding: 1em 0;
+
+            .tos_section {
+                display: flex;
+                align-items: center;
+            }
+        }
+    }
+
+    .left_section {
+        width: 40%;
+        background-image: url('@/assets/images/panama-city.webp');
+
+    }
+}
 </style>
