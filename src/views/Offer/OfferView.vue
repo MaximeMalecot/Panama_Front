49591c8955dc16a45f1eeac9dd923533e1e2180f
jsaleix <julian.saleix@gmail.com>
2023-02-07 17:46:59 +0100

    Adding sub modal in OfferView

--- src/views/Offer/OfferView.vue
+++ src/views/Offer/OfferView.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { reactive, ref, onMounted } from 'vue';
-import OfferApply from '@/components/Offers/OfferApply.vue';
-import Offer from '@/components/Offers/Offer.vue';
+import { reactive, ref, onMounted } from "vue";
+import OfferApply from "@/components/Offers/OfferApply.vue";
+import Offer from "@/components/Offers/Offer.vue";
 import ProjectService from "@/services/project.service";
-import { useRoute, useRouter } from 'vue-router';
-import { useAuthStore } from '../../stores/auth';
-import {ROLES} from "@/constants/roles";
+import { useRoute, useRouter } from "vue-router";
+import { useAuthStore } from "../../stores/auth";
+import { ROLES } from "@/constants/roles";
+import PremiumModal from "@/components/Premium/PremiumModal.vue";
 
 const authStore = useAuthStore();
 
@@ -13,60 +14,63 @@ const route = useRoute();
 const router = useRouter();
 
 const data = reactive({
-    "title": "Conception d'un site avec Shopify",
-    "tags": ["python", "htmlcss"],
-    "time": 1,
-    "price": 800,
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    title: "Conception d'un site avec Shopify",
+    tags: ["python", "htmlcss"],
+    time: 1,
+    price: 800,
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 });
 
 const project = ref({});
 const loading = ref(true);
+const showModal = ref(false);
+
+const handleApplication = () => {
+    console.log(authStore.userData);
+    if (!authStore.isConnected) {
+        console.log("you must be logged in");
+        router.push({ name: "login" });
+        return;
+        //router.push({ name: 'login' });
+    }
+
+    const { roles, isVerified } = authStore.userData;
+
+    if (!roles.includes(ROLES.FREELANCER_PREMIUM)) {
+        showModal.value = true;
+        console.log("you must be premium");
+        return;
+        //router.push({ name: 'login' });
+    }
+
+    if (!isVerified) {
+        console.log("you must be verified");
+        return;
+        //router.push({ name: 'login' });
+    }
+
+    console.log("handleApplication");
+};
 
 onMounted(async () => {
     const res = await ProjectService.getProject(route.params.id);
-    if (!res){
-        router.push({ name: 'offers' });
+    if (!res) {
+        router.push({ name: "offers" });
     }
     project.value = res;
     loading.value = false;
 });
-
-const handleApplication = () => {
-    console.log(authStore.userData);
-    if(!authStore.isConnected){
-        console.log("you must be logged in");
-        router.push({ name: 'login' });
-        return;
-        //router.push({ name: 'login' });
-    }
-    const { roles, isVerified } = authStore.userData;
-    
-    if( !roles.includes(ROLES.FREELANCER_PREMIUM) ){
-        console.log("you must be premium")
-        return;
-        //router.push({ name: 'login' });
-    }
-
-    if( !isVerified ){
-        console.log("you must be verified")
-        return;
-        //router.push({ name: 'login' });
-    }
-    
-    console.log('handleApplication');
-}
-
 </script>
 
 <template>
-    <main v-if="loading"  class="container">
-        Loading...
-    </main>
+    <main v-if="loading" class="container">Loading...</main>
     <main v-else class="offer">
         <div class="container">
             <section class="offer__header">
-                <RouterLink :to="{ name: 'offers' }" class="offer__header__link">⬅️ revenir aux offres</RouterLink>
+                <RouterLink :to="{ name: 'offers' }" class="offer__header__link"
+                    >⬅️ revenir aux offres</RouterLink
+                >
             </section>
             <section class="offer__content">
                 <Offer :data="project" />
@@ -75,6 +79,7 @@ const handleApplication = () => {
                 <OfferApply :handleClick="() => handleApplication()" />
             </section>
         </div>
+        <PremiumModal :close="() => (showModal = false)" v-show="showModal" />
     </main>
 </template>
 
@@ -82,11 +87,13 @@ const handleApplication = () => {
 .offer {
     &__header {
         padding: 2rem 0;
+        
         &__link {
             color: var(--text-gray);
             text-decoration: none;
         }
     }
+
     &__content {
         margin-bottom: 3rem;
     }
