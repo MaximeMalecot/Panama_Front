dc16eaab9f875acd7612139eeec8a7d7717c9fd9
jsaleix <julian.saleix@gmail.com>
2023-02-09 21:26:53 +0100

    Adding useFreelancerProjects hook + implementing it in DashboardProjects

--- src/views/Dashboard/DashboardProjects/DashboardProjectsView.vue
+++ src/views/Dashboard/DashboardProjects/DashboardProjectsView.vue
@@ -1,13 +1,19 @@
 <script setup>
-import { useOffersStore } from '@/stores/offers'
 import { useRouter } from 'vue-router';
 import OfferCard from '@/components/Offers/OfferCard.vue';
 import NoResults from '@/components/NoResults.vue';
+import { storeToRefs } from 'pinia';
+import useFreelancerProjects from "@/hooks/use-freelancer-projects";
+import { onMounted } from 'vue';
 
 const router = useRouter();
+const FreelancerProjectsStore = useFreelancerProjects();
+const { projects: offers } = storeToRefs(FreelancerProjectsStore);
 
-const OffersStore = useOffersStore();
-OffersStore.getOffers();
+onMounted(()=>{
+    FreelancerProjectsStore.fetchAllProjects();
+});
+
 </script>
 
 <template>
@@ -16,16 +22,16 @@ OffersStore.getOffers();
         <p>Vous retrouverez ici tous les projets qui vous sont assignés. Vous pourrez voir vos projets en cours ainsi
             que ceux que vous avez terminé.</p>
         <h2 class="projets__subtitle">Projets en cours</h2>
-        <div v-if="OffersStore.offers.length > 1" class="projets__list">
-            <OfferCard v-for="offer in OffersStore.offers" :offerData="offer" status="inprogress" hasLink
+        <div v-if="offers.length > 1" class="projets__list">
+            <OfferCard v-for="offer in offers" :offerData="offer" status="inprogress" hasLink
                 @click="router.push({ name: 'dashboard-project', params: { id: 1 } })"></OfferCard>
         </div>
         <div v-else class="projets__no-results">
             <NoResults>Vous ne vous êtes positionné sur aucune offre dernièrement.</NoResults>
         </div>
         <h2 class="projets__subtitle">Projets terminés</h2>
-        <div v-if="OffersStore.offers.length > 1" class="projets__list">
-            <OfferCard v-for="offer in OffersStore.offers" :offerData="offer" status="issued" hasLink
+        <div v-if="offers.length > 1" class="projets__list">
+            <OfferCard v-for="offer in offers" :offerData="offer" status="issued" hasLink
                 @click="router.push({ name: 'dashboard-project', params: { id: 1 } })"></OfferCard>
         </div>
         <div v-else class="projets__no-results">
