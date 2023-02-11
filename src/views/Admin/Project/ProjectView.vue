e58acf819b31bddd3ac58b975e2a2f54cb2d868e
MaximeMalecot <92maximemalecot@gmail.com>
2023-02-11 13:45:01 +0100

    Added form edit for project in status ACTIVE

--- src/views/Admin/Project/ProjectView.vue
+++ src/views/Admin/Project/ProjectView.vue
@@ -3,6 +3,10 @@ import { ref, onMounted, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import projectService from '@/services/project.service';
 import Btn from '../../../components/common/Btn.vue';
+import Input from "@/components/common/InputField.vue";
+import { PROJECT_STATUS } from "@/constants/status.js";
+import SearchFilters from '@/views/Dashboard/client-offer/SearchFilters.vue';
+import FilterItem from '@/components/common/FilterItem.vue';
 
 const route = useRoute();
 const id = route.params.id;
@@ -18,23 +22,83 @@ onMounted(async () => {
     }
 });
 
+const removeFilter = (payload) => {
+    const id = payload["@id"];
+    const tags = project.value.filters;
+    const newTags = tags.filter((t) => t["@id"] !== id);
+    project.value.filters = newTags;
+};
+
+const addFilter = (tagToAdd) => {
+    if (project.filters && project.filters.length > 0 && project.filters.find((t) => t["@id"] === tagToAdd["@id"])) return;
+    const tags = project.value.filters.length > 0 ? project.value.filters : [];
+    project.value.filters = [...tags, tagToAdd];
+};
+
+const updateProject = async (id, data) => {
+    const res = await projectService.updateProject(id, data);
+    if(res){
+        console.log(res);
+        project.value = res;
+    }
+}
+
+const updateProjectWrapper = () => {
+    let { name, description, filters } = project.value;
+    filters = filters.map((f) => f["@id"]);
+    const toSend = { name, description, filters}
+    updateProject(project.value.id, toSend);
+};
 
 </script>
 
 <template>
     <div class="container">
         <div class="header">
-            <h3>Project {{ project.name }}</h3>
-            <div>
-                <p>{{ project.description }}</p>
-                <p>status : {{ project.status }}</p>
-                <p>minPrice : {{ project.minPrice }}</p>
-                <p>maxPrice : {{ project.maxPrice }}</p>
-                <p>project duration : {{ project.length }}</p>
-                <p>createdAt : {{ project.createdAt }}</p>
-                <p>updatedAt : {{ project.updatedAt }}</p>
-                <Btn :type="'link'" :to="{ name: 'admin-project-edit', params: { id: id } }">Modifier</Btn>
-            </div>
+            <template v-if="project.status === PROJECT_STATUS.ACTIVE">
+                <!-- patch : name, description, filters -->
+                <h3>Project <Input type="text" v-model="project.name"/></h3> 
+                <div>
+                    <textarea v-model="project.description">{{ project.description }}</textarea>
+                    <p>status : {{ project.status }}</p>
+                    <p>minPrice : {{ project.minPrice }}</p>
+                    <p>maxPrice : {{ project.maxPrice }}</p>
+                    <p>project duration : {{ project.length }}</p>
+                    <p>createdAt : {{ project.createdAt }}</p>
+                    <p>updatedAt : {{ project.updatedAt }}</p>
+                </div>
+                <div v-if="project.filters && (project?.filters).length > 0" class="filters_items">
+                    <FilterItem
+                        v-for="(tag, index) in project.filters"
+                        :key="index"
+                        :data="tag"
+                        :id="index"
+                        :onClick="removeFilter"
+                    />
+                </div>
+                <SearchFilters :addFilter="addFilter" />
+                <Btn @click="updateProjectWrapper">Update</Btn>
+            </template>
+            <template v-else>
+                <h3>Project {{ project.name }}</h3> 
+                <div>
+                    <p>{{ project.description }}</p>
+                    <p>status : {{ project.status }}</p>
+                    <p>minPrice : {{ project.minPrice }}</p>
+                    <p>maxPrice : {{ project.maxPrice }}</p>
+                    <p>project duration : {{ project.length }}</p>
+                    <p>createdAt : {{ project.createdAt }}</p>
+                    <p>updatedAt : {{ project.updatedAt }}</p>
+                </div>
+                <div v-if="project.filters && (project?.filters).length > 0" class="filters_items">
+                    <FilterItem
+                        v-for="(tag, index) in project.filters"
+                        :key="index"
+                        :data="tag"
+                        :id="index"
+                    />
+                </div>
+            </template>
         </div>
         <div class="propositions" v-if="propositions">
             <h3>Propositions</h3>
